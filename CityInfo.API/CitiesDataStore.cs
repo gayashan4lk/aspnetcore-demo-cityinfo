fc15cafd7adb94685bd9f1e3841232373b45f848
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-10-01 23:03:43 +0530

    Add Cities API and refactor WeatherForecast code
    Introduced a `CitiesController` with endpoints to retrieve city data and a `CitiesDataStore` singleton to manage city information. Added a `CityDto` class to define city data structure.
    
    Refactored `WeatherForecastController` and `WeatherForecast` for improved readability and maintainability.
    
    Added `Microsoft.Extensions.ApiDescription.Server` NuGet package to support API description capabilities.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,38 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API;
+
+public class CitiesDataStore
+{
+    public List<CityDto> Cities { get; set; }
+
+    public static CitiesDataStore Instance { get; set; } = new CitiesDataStore();
+
+    public CitiesDataStore()
+    {
+        Cities = new List<CityDto>()
+        {
+            new CityDto()
+            {
+                Id = 1,
+                Name = "New York City",
+                Description = "The city that never sleeps"
+            },
+            new CityDto()
+            {
+                Id = 2,
+                Name = "Los Angeles",
+                Description = "The entertainment capital of the world"
+            },
+            new CityDto()
+            {
+                Id = 3,
+                Name = "Chicago",
+                Description = "The Windy City"
+            }
+
+        };
+
+
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetCities()
+    {
+        return new JsonResult(CitiesDataStore.Instance.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+        return new JsonResult(CitiesDataStore.Instance.Cities.FirstOrDefault(c => c.Id == id));
+    }
+    
+}
